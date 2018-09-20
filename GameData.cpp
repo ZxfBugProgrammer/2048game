712730e0e2e9481d44a6f9cb01e25be0ffeb4e85
linzihao <104664078@163.com>
2018-09-20 15:24:56 +0800

    增加gamedata类的方法

--- GameData.cpp
+++ GameData.cpp
@@ -1,1 +1,33 @@
 #include "GameData.h"
+#include <iostream>
+
+GameData::GameData() {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            num[i][j] = 0;
+    score = 0;
+}
+
+int GameData::get_position_number(int x, int y) {
+    return this->num[x][y];
+}
+
+void GameData::change_position_number(int x, int y, int p) {
+    num[x][y] = p;
+}
+
+int GameData::get_score() {
+    return 0;
+}
+
+int GameData::get_best_score() {
+    return 0;
+}
+
+void GameData::save_data(std::string path = "") {
+
+}
+
+void GameData::read_data(std::string path = "") {
+
+}
--- GameData.h
+++ GameData.h
@@ -1,9 +1,42 @@
+/*
+ * this class save all game data
+ */
 #ifndef INC_2048GAME_GAMEDATA_H
 #define INC_2048GAME_GAMEDATA_H
 
+#include <iostream>
 
 class GameData {
+public:
 
+    GameData();
+
+    //read game data from file
+    //or do nothing to begin a new game
+    void read_data(std::string);
+
+    //get grid (x,y) number from data
+    // 0 for empty
+    int get_position_number(int, int);
+
+    //change (x,y) number
+    void change_position_number(int, int, int);
+
+    //get my score
+    int get_score();
+
+    //get history best score
+    int get_best_score();
+
+    //save data to file
+    //default save to work directory
+    void save_data(std::string);
+
+private:
+    int num[4][4];
+    int score, my_best;
+    std::string local_name;
+    int local_best;
 };
 
 
