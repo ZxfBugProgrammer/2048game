cf9f5192cc772bfbd7e608265ffe6e85e8e22356
linzihao <104664078@163.com>
2018-09-20 08:30:05 +0800

    增加Control类文件

---
+++ Control.cpp
@@ -0,0 +1,14 @@
+/*
+ * This class control all the game
+ */
+
+#include "Control.h"
+#include <iostream>
+
+Control::Control() {
+
+}
+
+void Control::begin() {
+    std::cout << "Welcome" << std::endl;
+}
---
+++ Control.h
@@ -0,0 +1,17 @@
+//
+// Created by 林子豪 on 2018/9/20.
+//
+
+#ifndef INC_2048GAME_CONTROL_H
+#define INC_2048GAME_CONTROL_H
+
+
+class Control {
+public:
+    Control();
+
+    void begin();
+};
+
+
+#endif //INC_2048GAME_CONTROL_H
