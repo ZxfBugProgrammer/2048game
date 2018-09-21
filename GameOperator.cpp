3539fcf47dd99c9418e2248c986845fe38326087
linzihao <104664078@163.com>
2018-09-21 10:53:24 +0800

    增加win平台

--- GameOperator.cpp
+++ GameOperator.cpp
@@ -1,17 +1,29 @@
 #include "GameOperator.h"
 #include <iostream>
+
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#include <conio.h>
+#else
+
 #include <termios.h>
 #include <unistd.h>
-#include <stdio.h>
+
+#endif
 
 GameOperator::GameOperator() {
 }
 
 bool GameOperator::WaitMove() {
-    int ch=getkey();
+    int ch = getkey();
 }
 
 int GameOperator::getkey() {
+#ifdef _WIN32
+    return getch();
+#else
     struct termios oldattr, newattr;
     int ch;
     tcgetattr(STDIN_FILENO, &oldattr);
@@ -21,7 +33,7 @@ int GameOperator::getkey() {
     ch = getchar();
     tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
     return ch;
-
+#endif
 }
 
 int GameOperator::GetMoveNumber() {
