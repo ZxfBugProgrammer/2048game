01f24ff14295604e620ccd071e23d00a04b6c5c0
linzihao <104664078@163.com>
2018-09-21 11:02:34 +0800

    增加操作定义

---
+++ OperatorDef.h
@@ -0,0 +1,16 @@
+//
+// Created by 林子豪 on 2018/9/21.
+//
+
+#ifndef INC_2048GAME_OPERATORDEF_H
+#define INC_2048GAME_OPERATORDEF_H
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+#define QUIT 5
+#define YES 6
+#define NO 7
+
+#endif //INC_2048GAME_OPERATORDEF_H
