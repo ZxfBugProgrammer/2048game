fc53dff837b5483d3487f255c2e276ea68da13e4
zxf <1150123839@qq.com>
2018-09-21 11:48:17 +0800

    修改

--- Control.cpp
+++ Control.cpp
@@ -13,12 +13,15 @@ void Control::loop(GameData Data, Draw DrawUi, GameOperator Operator) {
         int Tempop = Operator.GetMoveNumber();
         if (Tempop == QUIT) {
             std::cout << "                Do you really want to quite the game?([Y]/[N])" << std::endl;
-            if (Operator.GetMoveNumber() == YES)
+            Tempop = Operator.GetMoveNumber();
+            if (Tempop == YES)
                 break;
-            else
-                Tempop = Operator.GetMoveNumber();
         }
-        Operator.Move(Data, Tempop);
+        if(Tempop != NO && Tempop != YES && Tempop != QUIT)
+        {
+            Operator.Move(Data, Tempop);
+            GameData.MakeNewNumber();
+        }
         DrawUi.ClearScreen();
     }
 }
